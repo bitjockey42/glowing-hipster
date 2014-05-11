34d7240516d20bcb4921a41f0b69b7a4cfaf41e3
hckr <hckr@eml.cc>
2014-05-11 17:32:14 -0500

    Make border a brighter green.

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -14,13 +14,13 @@ theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
 theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
+theme.fg_focus      = "#76EE00"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.border_width  = 2
 theme.border_normal = "#000000"
-theme.border_focus  = "#82B600"
+theme.border_focus  = "#76EE00"
 theme.border_marked = "#91231c"
 
 -- Disable taskbar icons
