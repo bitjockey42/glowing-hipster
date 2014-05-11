2f6bd47006ec4b84a2dd17a38a1f0e3340722171
hckr <hckr@eml.cc>
2014-05-11 16:58:16 -0500

    Added a bit more colour to the borders so I can actually see them.

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -18,9 +18,9 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.border_width  = 5
+theme.border_width  = 2
 theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
+theme.border_focus  = "#CC9393"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
